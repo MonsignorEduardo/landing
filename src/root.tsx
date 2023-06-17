7e5267e209442405196df00f1de4e8258f8e361f
Eduardo Gonzalez <monsignor.eduardo@gmail.com>
2023-06-17 21:40:24 +0200

    💄 Viewpoint

--- src/root.tsx
+++ src/root.tsx
@@ -19,6 +19,10 @@ export default component$(() => {
     <QwikCityProvider>
       <head>
         <meta charSet="utf-8" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1"
+        />
         <link rel="manifest" href="/manifest.json" />
       </head>
       <body lang="en">
